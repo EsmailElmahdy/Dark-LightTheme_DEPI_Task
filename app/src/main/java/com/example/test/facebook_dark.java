cd235a667317eb491a2d7a701e8de0baa82e43ac
Esmail Elmahdy <esmailelmahdy2002@gmail.com>
2024-08-10 15:17:17 +0300

    new commit

--- app/src/main/java/com/example/test/facebook_dark.java
+++ app/src/main/java/com/example/test/facebook_dark.java
@@ -1,8 +1,11 @@
-package com.example.newjavaproject;
+package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class facebook_dark extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class facebook_dark extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_dark);
+        Button light_btn = findViewById(R.id.light);
+        light_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(facebook_dark.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
